861ab63159296a5af1bd2944d6b01be753f4d5e5
Luis Enrique Suárez <luissuarezdev@gmail.com>
2017-10-15 18:49:28 -0400

    hello

--- src/xo/Board.java
+++ src/xo/Board.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package xo;
 
 import java.io.IOException;
@@ -17,10 +12,6 @@ import java.util.logging.Logger;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author luissuarez
- */
 public class Board extends javax.swing.JFrame {
 
     /* Socket Server */
